241522bcd04a92ce774d6d81fdbd24f69f300e85
Tony Rasa <trasa+github@meancat.com>
2015-01-07 18:23:45 -0800

    read a file of ascii cats and return one when url is /

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 import flask
+import codecs
+import random
 
 application = flask.Flask(__name__)
 
@@ -6,9 +8,26 @@ application = flask.Flask(__name__)
 #Make sure to remove this line before deploying to production.
 application.debug=True
 
+ascii_cats=[]
+
 @application.route('/')
-def hello_world():
-    return "Hello WORLD!!!"
+def random_cat():
+    return get_random_cat()
+
+def get_random_cat():
+    """ get a random cat from our ascii_cats collection """
+    if ascii_cats:
+        i =random.randint(0, len(ascii_cats)-1)
+        return ascii_cats[i]
+    else:
+        return None
+
+def load_cats():
+    """ read the cats file into a list """
+    global ascii_cats
+    with open('cats.txt') as f:
+        ascii_cats = f.read().splitlines()
 
 if __name__ == '__main__':
+    load_cats()
     application.run(host='0.0.0.0')
